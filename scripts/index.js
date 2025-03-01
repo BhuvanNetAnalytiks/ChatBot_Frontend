497a349dfc62cd97a60cd25016d67ae3384d1c0b
Sannidhii15 <sannidhis@netanalytiks.com>
2025-03-01 17:07:35 +0530

    Fixed the ticketing system issue

--- scripts/index.js
+++ scripts/index.js
@@ -21,7 +21,7 @@ chatContainer.addEventListener('click', (e) => {
 async function loadOrchestrationFile() {
     console.log('Loading Orchestration File...');
     try {
-        const response = await fetch('http://127.0.0.1:5002/orchestration.json');
+        const response = await fetch('http://127.0.0.1:5000/get_orchestration');
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
