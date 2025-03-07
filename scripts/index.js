86b49c74762214635c42d8cf3a7bca239f417f4f
BhuvanNetAnalytiks <bhuvans@netanalytiks.com>
2025-03-07 09:58:36 +0530

    Made changes in the in diasplaying text

--- scripts/index.js
+++ scripts/index.js
@@ -267,7 +267,7 @@ async function sendDataFetchRequest() {
         console.log(`${currentStep.function} API Response:`, result);
 
         if (response.ok) {
-            const successMessage = `Data fetched successfully! Response: ${JSON.stringify(result.result)}`;
+            const successMessage = `Data fetched successfully! ${JSON.stringify(result.result)}`;
             displayMessage(successMessage, 'bot');
         } else {
             console.error(`Error fetching data:`, result);
